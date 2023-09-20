eb97b3c83759a2f022d6fd984dabcfa22339944c
einatmor84 <144316208+einatmor84@users.noreply.github.com>
2023-09-20 21:08:25 +0300

    Update and rename Ctor.java to CtorAndStatic.java

--- CtorAndStatic.java
+++ CtorAndStatic.java
@@ -8,19 +8,23 @@ public class Main {
         student1.age = 50;*/
         System.out.printf("Student name: %s, last name: %s, age: %d\n", student1.firstName,student1.lastName, student1.age);
         Student student2 = new Student("Ester", "Shapira", 25);
-        System.out.printf("Student name: %s, last name: %s, age: %d\n", student2.firstName,student2.lastName, student2.age);
-
+        //System.out.printf("Student name: %s, last name: %s, age: %d\n", student2.firstName,student2.lastName, student2.age);
+        student2.ToString();
         Student student3 = new Student("a", "b");
         Student student4 = new Student();
 
+        student1.ToString();
+        System.out.println(Student.collageName); // calling static from class!
     }
 }
+
 
 
 public class Student {
     String firstName;
     String lastName;
     int age;
+    static String collageName = "Smart";
      Student(String firstName, String lastName, int age){  // constructor
         this.firstName = firstName;
         this.lastName = lastName;
@@ -33,4 +37,8 @@ public class Student {
      Student(){
          System.out.println("new Student has created");
     }
+    public void ToString(){
+        System.out.println("Student name: " + firstName + " "  + lastName + ", age:  " + age + ",  collage name: " + collageName );
+    }
+
 }
