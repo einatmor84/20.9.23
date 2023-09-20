f344b0071ca90c433c8542b95c04196481e8877c
einatmor84 <144316208+einatmor84@users.noreply.github.com>
2023-09-20 19:48:58 +0300

    Update Ctor.java

--- Ctor.java
+++ Ctor.java
@@ -15,3 +15,22 @@ public class Main {
 
     }
 }
+
+
+public class Student {
+    String firstName;
+    String lastName;
+    int age;
+     Student(String firstName, String lastName, int age){  // constructor
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+     Student(String firstName, String lastName){  // constructor
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+     Student(){
+         System.out.println("new Student has created");
+    }
+}
